e1ead22fff598f80c9f1b8a8525a1c20f7d22985
SongJeaHo <jeaho2552@gmail.com>
2022-08-19 16:44:44 +0900

    202208191644

---
+++ lib/components/allout_themes.dart
@@ -0,0 +1,40 @@
+import 'package:allout/components/allout_colors.dart';
+import 'package:flutter/material.dart';
+
+class AlloutThemes {
+  static ThemeData get mainTheme => ThemeData(
+        primarySwatch: AlloutColors.primaryMaterialColor,
+        scaffoldBackgroundColor: Colors.white,
+        splashColor: Colors.white,
+        brightness: Brightness.light,
+        fontFamily: 'Pretendard',
+        textTheme: _textTheme,
+      );
+
+  static const TextTheme _textTheme = TextTheme(
+    headline4: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w400,
+    ),
+    subtitle1: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+    ),
+    subtitle2: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    ),
+    bodyText1: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.w300,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+    ),
+    button: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w300,
+    ),
+  );
+}
